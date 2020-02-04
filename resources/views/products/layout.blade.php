83eb4cc745d89ac113c227667c3d74e9713a5364
Arif <asarifkhan08@gmail.com>
2020-02-04 15:19:54 +0600

    laravel crud

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 6 CRUD Application</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  
+<div class="container" style="margin-top:100px;border:2px solid black;">
+    @yield('content')
+</div>
+   
+</body>
+</html>
